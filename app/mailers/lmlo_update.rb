405006dcfc0460221ad61fb25b21e72ea00d0505
crowesn <crowesn@gmail.com>
2014-12-18 11:51:42 -0500

    fine-tune mailer

--- app/mailers/lmlo_update.rb
+++ app/mailers/lmlo_update.rb
@@ -1,8 +1,8 @@
 class LmloUpdate < ActionMailer::Base
-  default from: "lmlo@larry.libraries.uc.edu"
+  default from: "NO-REPLY@larry.libraries.uc.edu"
 
   def new_report(user)
     @user = user
-    mail(to: @user.email, subject: 'new lmlo items for review')
+    mail(to: @user.email, subject: 'New LMLO items for review')
   end
 end
