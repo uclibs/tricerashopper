e0d96cd1d8dd2f3e4c5410ef4dfa623470b1a643
James Van Mil <james.vanmil@gmail.com>
2015-01-24 07:29:02 -0500

    Draft Serials index view

--- app/controllers/serials_controller.rb
+++ app/controllers/serials_controller.rb
@@ -12,55 +12,6 @@ class SerialsController < ApplicationController
   def show
   end
 
-  # GET /serials/new
-  def new
-    @serial = Serial.new
-  end
-
-  # GET /serials/1/edit
-  def edit
-  end
-
-  # POST /serials
-  # POST /serials.json
-  def create
-    @serial = Serial.new(serial_params)
-
-    respond_to do |format|
-      if @serial.save
-        format.html { redirect_to @serial, notice: 'Serial was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @serial }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @serial.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /serials/1
-  # PATCH/PUT /serials/1.json
-  def update
-    respond_to do |format|
-      if @serial.update(serial_params)
-        format.html { redirect_to @serial, notice: 'Serial was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @serial.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /serials/1
-  # DELETE /serials/1.json
-  def destroy
-    @serial.destroy
-    respond_to do |format|
-      format.html { redirect_to serials_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_serial
