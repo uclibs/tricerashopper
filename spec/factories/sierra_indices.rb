8a82a940b0d7ce78d7474b8923c00509629e7574
James Van Mil <james.vanmil@gmail.com>
2015-03-14 15:43:31 -0400

    Draft specs

--- spec/factories/sierra_indices.rb
+++ spec/factories/sierra_indices.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :sierra_index do
-    record_type "MyString"
-    record_num 1
+    record_type "b"
+    record_num 12345678
     last_checked "2015-03-14 15:15:30"
   end
 end
--- spec/models/sierra_index_spec.rb
+++ spec/models/sierra_index_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe SierraIndex do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject { SierraIndex.new }
+
+  it { should respond_to(:record_type) }
+  it { should respond_to(:record_num) }
+  it { should respond_to(:last_checked) }
+
+  it "has a valid factory" do
+    expect(build(:sierra_index)).to be_valid
+  end
+
+  it "is invalid without record type" do
+    expect(build(:sierra_index, record_type: nil )).to be_invalid
+  end
+
+  it "is invalid without record number" do
+    expect(build(:sierra_index, record_num: nil )).to be_invalid
+  end
+
+  it "is valid with record type 'o'" do
+    expect(build(:sierra_index, record_type: 'o' )).to be_valid
+  end
+
+  it "is valid with record type 'b'" do
+    expect(build(:sierra_index, record_type: 'b' )).to be_valid
+  end
+
+  it "is valid with record type 'c'" do
+    expect(build(:sierra_index, record_type: 'c' )).to be_valid
+  end
+
+  it "is valid with record type 'i'" do
+    expect(build(:sierra_index, record_type: 'i' )).to be_valid
+  end
+
+  it "is invalid without record type [obci]" do
+    expect(build(:sierra_index, record_type: 'z' )).to be_invalid
+  end
 end
