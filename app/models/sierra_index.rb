2e8c36ae985743784773384c83fd6953e678bb31
James Van Mil <james.vanmil@gmail.com>
2015-03-14 15:49:51 -0400

    Specs pass

--- app/models/sierra_index.rb
+++ app/models/sierra_index.rb
@@ -1,2 +1,5 @@
 class SierraIndex < ActiveRecord::Base
+  validates :record_type, presence: true
+  validates :record_type, inclusion: { in: %w(o b c i) }
+  validates :record_num, presence: true
 end
