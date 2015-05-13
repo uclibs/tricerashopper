94512f935d5aa6f949b34ddd93a6744aa5e53e49
crowesn <crowesn@gmail.com>
2015-05-13 14:22:14 -0400

    create rdate spec

---
+++ spec/models/blank_r_date_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe Problem do
+
+  subject { Problem.new }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:record_num) }
+  it { should respond_to(:record_type) }
+  it { should respond_to(:description) }
+
+  it "is valid with title" do
+    expect(build(:problem, title: 'My Title')).to be_valid
+  end
+
+  it "is invalid without title" do
+    expect(build(:problem, title: nil)).to be_invalid
+  end
+
+  it "is valid with record number" do
+    expect(build(:problem, record_num: 12345678)).to be_valid
+  end
+
+  it "is invalid without record number" do
+    expect(build(:problem, record_num: nil)).to be_invalid
+  end
+
+  it "is valid with record_type" do
+    expect(build(:problem, record_type: 'o')).to be_valid
+  end
+
+  it "is invalid wihtout record_type" do 
+    expect(build(:problem, record_type: nil)).to be_invalid
+  end
+
+  it "is valid with description" do
+    expect(build(:problem, description: 'My Text')).to be_valid
+  end
+
+  it "is invalid without description" do
+    expect(build(:problem, description: nil)).to be_invalid
+  end
+end
