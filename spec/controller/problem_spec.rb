a4d6d7be1c66721afe50f9f208a2412b5dfdd721
James Van Mil <james.vanmil@gmail.com>
2015-06-09 09:34:42 -0400

    Outline spec for audit

---
+++ spec/controller/problem_spec.rb
@@ -0,0 +1,12 @@
+describe "ProblemsController" do
+  describe "#audit" do
+    it "should run the query as the subclassed type" do
+    end
+
+    it "should delete the resource if the query returns false" do
+    end
+
+    it "should not delete the resource if the query returns true" do
+    end
+  end
+end
