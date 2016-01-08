acdecfb86fa15af0b56c53b25b05fd2e372505a0
James Van Mil <james.vanmil@gmail.com>
2016-01-08 14:13:10 -0500

    refactor order record QC job to better handle multiple problem types

--- app/jobs/order_record_qc.rb
+++ app/jobs/order_record_qc.rb
@@ -1,44 +1,52 @@
 class OrderRecordQc
   @queue = :order_qc
 
+  ORDER_RECORD_PROBLEMS = [ BlankRdate, CancelledByVendor ]
+
+  def self.problem_types
+    ORDER_RECORD_PROBLEMS
+  end
+
+  RECORD_TYPE = 'o'
+  def self.record_type
+    'o'
+  end
+
   def self.perform(record_num)
     unless deleted?(record_num)
-      if problem_exist?(record_num)
-        problem_review(record_num)
-      else
-        blank_r_date(record_num)
-        cancelled_by_vendor(record_num)
-      end 
+      problem_types.each do |type|
+        if problem_exist?(record_num, type)
+          problem_review(record_num, type)
+        else
+          create_problem(record_num, type)
+        end 
+      end
       update_index_date(record_num)
     end
   end
 
   private
 
-  def self.blank_r_date(record_num)
-      BlankRdate.create(record_num: record_num)
-  end
- 
-  def self.cancelled_by_vendor(record_num)
-      CancelledByVendor.create(record_num: record_num)
+  def self.create_problem(record_num, type)
+      type.create(record_num: record_num)
   end
   
-  def self.problem_exist?(record_num)
-    if Problem.where(record_num: record_num).presence
+  def self.problem_exist?(record_num, type)
+    if type.where(record_num: record_num).presence
       true
     else
       false
     end
   end
  
-  def self.problem_review(record_num)
-    records = Problem.where(record_num: record_num)
+  def self.problem_review(record_num, type)
+    records = type.where(record_num: record_num)
     records.each do |problem| 
       problem.destroy unless problem.valid? 
     end
   end
 
-  def self.deleted?(record_num, record_type = 'o')
+  def self.deleted?(record_num, type = record_type)
     if SierraIndexHelper.record_deleted?(record_num, record_type)
       SierraIndexHelper.destroy_index_entry(record_num, record_type)
       true
@@ -47,7 +55,7 @@ class OrderRecordQc
     end
   end
 
-  def self.update_index_date(record_num, record_type = 'o')
+  def self.update_index_date(record_num, type = record_type)
     SierraIndexHelper.update_date(record_num, record_type)
   end
 end
--- spec/jobs/order_record_qc_spec.rb
+++ spec/jobs/order_record_qc_spec.rb
@@ -4,56 +4,66 @@ describe OrderRecordQc do
   describe "#perform" do
     Resque.inline = true
 
-    context "BlankRdate already exists" do 
-      before(:each) do
-        prob = FactoryGirl.build_stubbed(:problem)
+    context "Problem already exists" do 
+      let(:problem) { FactoryGirl.create(:problem) }
+
+      before do
         OrderView.stub_chain(:where, :first, :record_metadata, :deletion_date_gmt).and_return(nil)
-        Problem.stub(:where).and_return([prob])
+        BlankRdate.stub(:where).and_return([problem])
       end  
 
-        it "should not create" do
-          BlankRdate.should_not receive(:create)
-          Resque.enqueue(OrderRecordQc, 100001)
-        end
-      end
+      it "should handle duplicate problems appropriately" do
+        ## skipping duplicate
+        BlankRdate.should_not receive(:create)
 
-    context "order record has not been deleted" do
-      before(:each) do
-        OrderView.stub_chain(:where, :first, :record_metadata, :deletion_date_gmt).and_return(nil)
-        BlankRdate.stub(:create).and_return(true)
-      end
+        ## checking problem is valid
+        problem.should receive(:valid?)
 
-      it "should run problem tests and update the index" do
-        ## should update the SierraIndex.last_checked date
-        SierraIndexHelper.should receive(:update_date).with(100001, 'o')
-      
-        ## should run the BlankRdate test
-        BlankRdate.should receive(:create)
-
-        ## should run the CancelledByVendor test
+        ## creating another for the same record
         CancelledByVendor.should receive(:create)
         Resque.enqueue(OrderRecordQc, 100001)
       end
     end
 
-    context "order record has been deleted" do
-      before(:each) do
-        OrderView.stub_chain(:where, :first, :record_metadata, :deletion_date_gmt).and_return(DateTime.parse('2001-01-06'))
+    context "Problem does not exist" do
+      context "order record has not been deleted" do
+        before do
+          OrderView.stub_chain(:where, :first, :record_metadata, :deletion_date_gmt).and_return(nil)
+          BlankRdate.stub(:create).and_return(true)
+        end
+
+        it "should run problem tests and update the index" do
+          ## should update the SierraIndex.last_checked date
+          SierraIndexHelper.should receive(:update_date).with(100001, 'o')
+        
+          ## should run the BlankRdate test
+          BlankRdate.should receive(:create)
+
+          ## should run the CancelledByVendor test
+          CancelledByVendor.should receive(:create)
+          Resque.enqueue(OrderRecordQc, 100001)
+        end
       end
 
-      it "should delete the index entry" do
-        SierraIndexHelper.should receive(:destroy_index_entry).with(100001, 'o')
-        Resque.enqueue(OrderRecordQc, 100001)
-      end
+      context "order record has been deleted" do
+        before(:each) do
+          OrderView.stub_chain(:where, :first, :record_metadata, :deletion_date_gmt).and_return(DateTime.parse('2001-01-06'))
+        end
 
-      it "should not run the BlankRdate test" do
-        BlankRdate.should_not receive(:create)
-        Resque.enqueue(OrderRecordQc, 100001)
-      end
- 
-      it "should not run the Problem Review test" do
-        Problem.should_not receive(:where)
-        Resque.enqueue(OrderRecordQc, 100001)
+        it "should delete the index entry" do
+          SierraIndexHelper.should receive(:destroy_index_entry).with(100001, 'o')
+          Resque.enqueue(OrderRecordQc, 100001)
+        end
+
+        it "should not run a problem test" do
+          BlankRdate.should_not receive(:create)
+          Resque.enqueue(OrderRecordQc, 100001)
+        end
+   
+        it "should not run the Problem Review test" do
+          Problem.should_not receive(:where)
+          Resque.enqueue(OrderRecordQc, 100001)
+        end
       end
     end
   end
