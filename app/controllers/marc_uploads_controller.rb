9114c3f91f37d7b9cbc5037d0533cc965a653746
crowesn <crowesn@gmail.com>
2016-04-20 12:45:57 -0400

    tweak arkive invoice process

--- app/controllers/marc_uploads_controller.rb
+++ app/controllers/marc_uploads_controller.rb
@@ -10,7 +10,7 @@ def create
      File.open(@path, "wb") { |f| f.write(params[:upload][:file].read) }
      s = Roo::Excelx.new(@path)
      File.delete(@path)
-     redirect_to marc_uploads_path and flash[:notice] = 'Columns must be in order: Label, Catalog, Title, UPC, Format, Invoice, Price' and return if s.row(1) != ['Label', 'Catalog', 'Title', 'UPC', 'Format', 'Invoice', 'Price']
+     redirect_to marc_uploads_path and flash[:notice] = 'Columns must be in order: Label, Catalog, Title, UPC, Format, Invoice, Price' and return if s.row(1) != ['Label', 'Catalog', 'Title', 'UPC', 'Price', 'Invoice', 'Format']
 
      writer = MARC::Writer.new("#{directory}/#{DateTime.now.strftime('%Y%m%d')}_#{name.gsub('xlsx', 'mrc').gsub(' ', '')}")
        s.drop(1).each do |row|
@@ -18,9 +18,9 @@ def create
        record.append(MARC::DataField.new('024', '0', '0', ['a', row[3].to_s]))
        record.append(MARC::DataField.new('245', '0', '0', ['a', row[2].to_s]))
        record.append(MARC::DataField.new('260', ' ', ' ', ['b', row[0].to_s]))
-       record.append(MARC::DataField.new('300', ' ', ' ', ['a', row[4].to_s]))
+       record.append(MARC::DataField.new('300', ' ', ' ', ['a', row[6].to_s]))
        record.append(MARC::DataField.new('961', ' ', ' ', ['d', "Invoice # #{row[5].to_i.to_s}"]))
-       record.append(MARC::DataField.new('980', ' ', ' ', ['a', Time.now.strftime("%y%m%d")], ['b', row[6].to_s.gsub('.', '')], ['e', row[6].to_s.gsub('.', '')], ['f', row[5].to_i.to_s], ['g', '1']))
+       record.append(MARC::DataField.new('980', ' ', ' ', ['a', Time.now.strftime("%y%m%d")], ['b', row[4].to_s.gsub('.', '')], ['e', row[4].to_s.gsub('.', '')], ['f', row[5].to_i.to_s], ['g', '1']))
        record.append(MARC::DataField.new('981', ' ', ' ', ['b', 'vcnfi'], ['c', 'UCM']))
        writer.write(record)
      end
